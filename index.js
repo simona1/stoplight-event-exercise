24c23dd09034f1f4b8a50617a66d70a4fa2ef3e4
Stela Palii <scientia.ssp@gmail.com>
2017-04-29 15:55:51 -0700

    Completed stoplight exercise

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+
 (function() {
   'use strict';
+  const buttons = document.querySelectorAll('div.button');
 
-  // YOUR CODE HERE
+  buttons.forEach(button => {
+    button.addEventListener('click', toggleColor);
+    button.addEventListener('mouseenter', onMouseEnter);
+    button.addEventListener('mouseleave', onMouseLeave);
+  });
+
+  function toggleColor(event) {
+    const text = event.target.textContent;
+    const name = text.toLowerCase();
+    const turnedOn =
+      document.getElementById(name + 'Light').classList.toggle(name);
+    console.log(text + ' bulb ' + (turnedOn ? 'on' : 'off'));
+  }
+
+  function onMouseEnter(event) {
+    console.log('Entered ' + event.target.textContent + ' button');
+  }
+
+  function onMouseLeave(event) {
+    console.log('Left ' + event.target.textContent + ' button');
+  }
 })();
